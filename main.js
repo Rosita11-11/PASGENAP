9c593a8bda078a09480ac50e8361b44d89b749e2
rosita11-11 <itaaar44@gmail.com>
2024-07-24 09:55:03 +0700

    update

--- main.js
+++ main.js
@@ -13,9 +13,9 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js"
 const firebaseConfig = {
   apiKey: "AIzaSyBm9HdJ92vSLrKRclI6Z2J4bmvlFgR4AuU",
-  authDomain: "mang-yana.firebaseapp.com",
-  projectId: "mang-yana",
-  storageBucket: "mang-yana.appspot.com",
+  authDomain: "rosita.firebaseapp.com",
+  projectId: "rosita",
+  storageBucket: "rosita.appspot.com",
   messagingSenderId: "1094982396668",
   appId: "1:1094982396668:web:2d103526ab40a59efc0579",
   measurementId: "G-MF48P7VG5P"
